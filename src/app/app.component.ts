91c893a91a1a6de14bf1a9f4092586120c764273
Andrii Rodzyk <rodzyk@programmer.net>
2024-01-29 23:59:27 +0200

    add manifest-uk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+const MANIFESTS = {
+  en: "manifest.webmanifest",
+  uk: "manifest-uk.webmanifest"
+}
 
 @Component({
   selector: 'app-root',
@@ -10,7 +15,26 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
- 
+  lang: string = localStorage.getItem("lang") ?? "en"
+
+  linkElement: WritableSignal<HTMLElement | null> = signal(document.querySelector('link[rel="manifest"]'))
+
+  constructor() {
+    this.changeManifest();
+  }
+
+  toggleLang() {
+    this.lang = this.lang == 'en' ? 'uk' : 'en';
+
+    localStorage.setItem("lang", this.lang)
+
+    this.changeManifest()
+  }
+
+  changeManifest() {
+    this.linkElement()?.setAttribute('href', MANIFESTS[this.lang as keyof { en: string; uk: string; }]);
+  }
+
   registerProtocolHandler() {
     (navigator as any).registerProtocolHandler("web+hellopwa", "/%s")
   }
